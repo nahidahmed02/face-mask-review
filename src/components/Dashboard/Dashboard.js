0d540106350a8a688542b06b312bb568815b02b1
unknown <ahmednahid1995@gmail.com>
2022-04-04 21:26:11 +0600

    line chart implimented

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
         <div>
+            <div>
+                <h4 className='text-center'>Month Wise Revenue</h4>
+                <LineChart width={600} height={400} data={data}>
+                    <Line dataKey={'revenue'}></Line>
+                    <XAxis dataKey={'month'}></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
 
+            <div>
+
+            </div>
         </div>
     );
 };
