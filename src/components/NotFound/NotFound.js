4cc96ed6761555b3a91b1f0cff8461c4f74d7e89
unknown <ahmednahid1995@gmail.com>
2022-04-04 07:41:28 +0600

    json data & 404 added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <img style={{ 'width': '55%' }} src="../../images/404.jpg" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
