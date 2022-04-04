c0001199ab304b9b1058226b7815dba0076769ff
unknown <ahmednahid1995@gmail.com>
2022-04-04 20:25:15 +0600

    second Q&A added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -5,7 +5,9 @@ const Blogs = () => {
         <div className='mx-5 my-4'>
             <h3 className="bg-success p-2">Q: What is the purpose of context API?</h3>
             <p className="bg-light p-2">A: The purpose of context API is to send data directly to a targeted component without sharing it with any other component. We use props to send data from one component to another. In that process, if we want to send data from a grand component to a child component, we have to send data to every component between these two. It is called prop drilling. To avoid prop drilling we use context API. Context API only sends data to the targeted component.</p>
-
+            <br />
+            <h3 className="bg-success p-2">Q: What is semantic tag?</h3>
+            <p className="bg-light p-2">A:  Tags that clearly describe their meaning in a way that both human and machine can understand them are called semantic tag. Semantic tags provide additional information to the browser. By that, the browser understands the roles of that tags. It is used to break the page into identified parts. The semantic tag offers a better user experience. Some semantic tags are header, nav, section, article, footer, details.</p>
 
         </div>
     );
