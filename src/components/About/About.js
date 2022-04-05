cb40348f123734c44b1054e0298e6f975bcd10c0
unknown <ahmednahid1995@gmail.com>
2022-04-05 22:35:24 +0600

    bean is here

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-
+        <div className='text-center'>
+            <h2>Wanna know more!!!</h2>
+            <img style={{ width: '50%' }} src="../../images/mr-bean-rowat-atkinson.gif" alt="" />
         </div>
     );
 };
